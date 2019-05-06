252e2fcd1855acb72071926601da5149319b8e03
Bjarni <bjarnib18@ru.is>
2019-05-06 16:44:22 +0000

    added 'realtors' app

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.realtors, name="realtors"),
     path('', views.index, name="users-index"),
     path('realtors/', views.realtors, name="realtors"),
 ]
