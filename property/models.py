1987ae690e64da9ca8022b1825b0b3619b60b8cc
AronU <aronu97@hotmail.com>
2019-05-06 14:19:10 +0000

    Property model work begun

--- property/models.py
+++ property/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Property(models.model):
+    street_name = models.CharField(max_length=255)
+    street_number = models.IntegerField()
+    property_description = models.CharField(2000)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    size = models.IntegerField()
+    rooms = models.IntegerField()
+    price = models.FloatField()
+
+class PropertyImage(models.model):
+    image = models.CharField(max_length=999)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE())
