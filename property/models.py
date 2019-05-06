896d754e5a44157d6904cd583860be67304a8233
AronU <aronu97@hotmail.com>
2019-05-06 15:58:04 +0000

    Property models and 0001_initial.py

--- property/models.py
+++ property/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Property(models.model):
-    street_name = models.CharField(max_length=255)
-    street_number = models.IntegerField()
-    property_description = models.CharField(2000)
-    zip = models.CharField(max_length=10)
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    size = models.IntegerField()
-    rooms = models.IntegerField()
-    price = models.FloatField()
 
-class PropertyImage(models.model):
+
+class Property(models.Model):
+    street_name = models.CharField(max_length=255)
+    street_number = models.CharField(max_length=10)
+    property_description = models.CharField(max_length=2000)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, blank=True)
+    size = models.IntegerField()
+    rooms = models.IntegerField(blank=True)
+    price = models.FloatField()
+    #TODO add realtor foreign key
+
+
+class PropertyImage(models.Model):
     image = models.CharField(max_length=999)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE())
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
