6c29878d2b29b8e89656f72fc405ac38b61b5097
Bjarni <bjarnib18@ru.is>
2019-05-07 16:20:28 +0000

    editing the realtor files in templates to be able to display realtors on the realtor page

--- realtor/views.py
+++ realtor/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'realtor/realtor.html')
+    context = {'realtors': Realtor.objects.all().order_by('user.first_name')}
+    return render(request, 'realtor/realtor.html',context)
+
 
 def get_realtor_by_id(request, id):
     return render(request, 'realtor/details.html', {
