73a67f9af2df28c4965f26759d6fdd34be2a1c23
AronU <aronu97@hotmail.com>
2019-05-10 14:03:10 +0000

    User history progress

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
+from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from user.views import update_history
 from . import views
 
 urlpatterns = [
@@ -7,5 +9,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    url(r'^update_history/', update_history),
 ]
