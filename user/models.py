cc6eb970918ab99bc394b5fbabb37774f9bcd586
AronU <aronu97@hotmail.com>
2019-05-13 12:47:44 +0000

    cart added to db

--- user/models.py
+++ user/models.py
@@ -26,6 +26,14 @@ class History(models.Model):
         unique_together = (("user", "property"),)
 
 
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("user", "property"),)
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
