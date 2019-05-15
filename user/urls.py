40a4538b2a00dd4084a24be8cbfaeb9566ea8c07
gunnarig <gunnaringiberg@gmail.com>
2019-05-15 11:46:38 +0000

    Upload picture

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name="users-index"),
@@ -12,4 +15,4 @@ urlpatterns = [
     path('edit/', views.edit_profile, name="edit_profile"),
     path('cart/', views.cart, name="cart"),
     url(r'^password/$', views.change_password, name='change_password'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
