cafdb96ebec654fb1d279f834666d8381541c7de
Rúnar Bjarkason <runar16@ru.is>
2019-05-17 10:48:05 +0000

    added admin page

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, History, Cart, Favourite
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(History)
+admin.site.register(Cart)
+admin.site.register(Favourite)
